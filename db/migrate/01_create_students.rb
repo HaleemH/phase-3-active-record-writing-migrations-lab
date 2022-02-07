1946b26d907e1ef142863dd0d5748acacd707ad0
Haleem Hamid <hhamidbiz@gmail.com>
2022-02-06 17:33:20 -0800

    commit tree 02ade5336de80843b5e4d49c615693d0da0aa237

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+      t.string :grade
+      t.string :birthdate
+    end
   end
 end
